0ddd13faf840f0447d2687c3ffcf29d0fe222a57
LAPTOP-6JT3A2QV\noel2 <noel25_1594@hotmail.com>
2019-04-02 12:32:37 -0600

    Modified routes for heroku

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
         this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://rocky-sierra-14798.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://rocky-sierra-14798.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
